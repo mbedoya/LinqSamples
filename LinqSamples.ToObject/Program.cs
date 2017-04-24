518d90b3135316e4dc13d827d63b9f222da36ccb
mbedoya <mauricio.bedoya@gmail.com>
2017-04-24 13:20:57 -0500

    Initial Commit - ToObject

---
+++ LinqSamples.ToObject/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace LinqSamples.ToObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                var processes =
+                from p in Process.GetProcesses()
+                orderby p.WorkingSet64 descending
+                select new { p.ProcessName, p.TotalProcessorTime, p.WorkingSet64 };
+
+                foreach (var item in processes)
+                {
+                    Console.WriteLine("{0, -25} {1, 12} {2, 20:N0}", item.ProcessName, item.TotalProcessorTime, item.WorkingSet64);
+                }
+            }
+            catch
+            {
+               
+            }            
+
+            Console.ReadKey();
+        }
+    }
+}
